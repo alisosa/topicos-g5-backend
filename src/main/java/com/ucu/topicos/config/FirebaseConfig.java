b4bfd44f9abf9d755d5afd4362a4117692b5858d
Nicolas Prantl <53955470+nicolasprantl@users.noreply.github.com>
2023-11-21 02:21:49 -0300

    mail config

--- src/main/java/com/ucu/topicos/config/FirebaseConfig.java
+++ src/main/java/com/ucu/topicos/config/FirebaseConfig.java
@@ -17,19 +17,17 @@ public class FirebaseConfig {
 
     @PostConstruct
     public void initialize() {
-        try {
-            FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase.json");
-
+        try (FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase.json")) {
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
 
             if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
-                logger.info("Firebase ha sido inicializado");
+                logger.info("Firebase has been initialized");
             }
         } catch (IOException e) {
-            logger.error("Error al inicializar Firebase: {}", e.getMessage(), e);
+            logger.error("Error initializing Firebase: {}", e.getMessage(), e);
         }
     }
 }
